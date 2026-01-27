34acba8428238f74319df66c1be6087d23b234b7
Mythic3011 <42365807+mythic3011@users.noreply.github.com>
2026-01-27 08:48:12 +0800

    refact(auth): remove session warning and status messages from login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -18,40 +18,6 @@
                 </p>
             </div>
 
-            @if(session('warning'))
-                <div class="rounded-md bg-yellow-50 border border-yellow-200 p-4">
-                    <div class="flex">
-                        <div class="flex-shrink-0">
-                            <svg class="h-5 w-5 text-yellow-400" viewBox="0 0 20 20" fill="currentColor">
-                                <path fill-rule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clip-rule="evenodd" />
-                            </svg>
-                        </div>
-                        <div class="ml-3">
-                            <p class="text-sm font-medium text-yellow-800">
-                                {{ session('warning') }}
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            @endif
-
-            @if(session('status'))
-                <div class="rounded-md bg-green-50 border border-green-200 p-4">
-                    <div class="flex">
-                        <div class="flex-shrink-0">
-                            <svg class="h-5 w-5 text-green-400" viewBox="0 0 20 20" fill="currentColor">
-                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
-                            </svg>
-                        </div>
-                        <div class="ml-3">
-                            <p class="text-sm font-medium text-green-800">
-                                {{ session('status') }}
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            @endif
-
             <div class="bg-white shadow-md rounded-lg p-8">
                 <form class="space-y-6" action="{{ route('login') }}" method="POST">
                     @csrf
